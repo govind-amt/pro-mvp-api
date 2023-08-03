0f3cb897d6cc3c7ed94e3ef9261dc7595571f35a
Govind Krishna <govind@corp.amt.in>
2023-08-03 23:54:59 +0530

    login api initial changes

--- libs/auth/src/lib/auth.service.ts
+++ libs/auth/src/lib/auth.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, Logger} from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async login(username: string | undefined, password: string | undefined){
+    Logger.debug("username and password: ", username, password);
+    return {
+      status: 200,
+      info: 'OK'
+    }
+  }
+}
