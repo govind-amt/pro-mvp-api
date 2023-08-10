a691f06a6de25179e6cb4798e49ffa1a4dd2bfff
Govind Krishna <govind@corp.amt.in>
2023-08-10 16:15:29 +0530

    added jwt in auth module

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports:[
+    JwtModule.register({
+      global: true,
+      secret: "BRING_FROM_CONSTANTS_FILE",
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [],
