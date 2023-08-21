b7c0db75d565c3c19f20d21c20af49d04235b5f6
anumon <anumon@amt.in>
2023-08-21 14:43:16 +0530

    centryInstallation

--- libs/auth/src/lib/auth.controller.ts
+++ libs/auth/src/lib/auth.controller.ts
@@ -2,7 +2,10 @@ import {Body, Controller , Post} from '@nestjs/common';
 import {ApiTags} from "@nestjs/swagger";
 import {AuthService} from "./auth.service";
 import {LoginDto} from "@pro-fuel-trace-api/data-transfer-object";
+import { UseInterceptors } from '@nestjs/common';
+import {SentryInterceptor} from "@pro-fuel-trace-api/configuration";
 
+@UseInterceptors(SentryInterceptor)
 @ApiTags('Auth')
 @Controller()
 export class AuthController {
